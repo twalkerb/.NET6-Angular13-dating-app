ca0704c1bf618502cd2a97ffc8e6020a02a9685a
gerard singian <gerard.singian@outlook.com>
2022-05-08 14:42:51 +0800

    adding validation

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
