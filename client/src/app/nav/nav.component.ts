d7b5ee0f50ee059b44f28c7cf6506d5d02973425
gerard singian <gerard.singian@outlook.com>
2022-05-09 16:46:51 +0800

    injecting services to the components

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,15 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-model: any = {};
-  constructor() { }
+  model: any = {};
+  loggedIn: boolean;
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
 
   }
 
   login() {
-    console.log(this.model);
+    // console.log(this.model);
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      this.loggedIn = true;
+    }, error => {
+      console.log(error);
+    })
   }
 
 }
