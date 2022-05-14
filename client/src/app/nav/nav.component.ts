fe5064f3588799bd16425e40f2fc6dcf22c72564
gerard singian <gerard.singian@outlook.com>
2022-05-14 13:33:52 +0800

    routing in code and add ngx toaster

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -11,7 +12,7 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
 
@@ -20,7 +21,7 @@ export class NavComponent implements OnInit {
   login() {
     // console.log(this.model);
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
     })
@@ -28,7 +29,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-
+    this.router.navigateByUrl('/');  
   }
 
 
