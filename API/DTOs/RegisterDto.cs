f3395e61a1f4cf1df04ca41525a1b58a7f0be88b
gerard singian <gerard.singian@outlook.com>
2022-05-15 00:13:52 +0800

    Handling Server Errors-

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,6 +11,7 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
