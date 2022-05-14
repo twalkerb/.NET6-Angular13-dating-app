cbbd8bd787136de980ec041a49f17617ed40ce1c
gerard singian <gerard.singian@outlook.com>
2022-05-14 15:03:27 +0800

    Bootswathc theme

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,11 +9,19 @@ import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'members', component: MemberListComponent, canActivate: [AuthGuard]},
-  {path:'members/:id', component: MemberDetailComponent},
-  {path:'lists', component: ListsComponent},
-  {path:'messages', component: MessagesComponent},
-  {path:'**', component: HomeComponent, pathMatch: 'full'},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path:'members', component: MemberListComponent, canActivate: [AuthGuard]},
+      {path:'members/:id', component: MemberDetailComponent},
+      {path:'lists', component: ListsComponent},
+      {path:'messages', component: MessagesComponent},
+    ]
+  },
+  
+  {path:'**', component: HomeComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
