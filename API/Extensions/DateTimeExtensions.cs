1c403099285cc1f286d009e20b37e35f6e2beb46
gerard singian <gerard.singian@outlook.com>
2022-05-15 23:59:25 +0800

    Addding datetime extensionm to calculate the age

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dob.Year;
+            if (dob.Date > today.AddYears(-age)) age--;
+            return age; 
+        }
+    }
+}
