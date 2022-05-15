5ced241c3d5a231a279cb6dd8f99fabf8245f66e
gerard singian <gerard.singian@outlook.com>
2022-05-15 18:31:30 +0800

    Validation Errors

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestErrorsComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -48,6 +50,7 @@ export class TestErrorsComponent implements OnInit {
       console.log(response);
     }, error => {
       console.log(error);
+      this.validationErrors = error;
     })
   }
 
