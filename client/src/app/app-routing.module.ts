0dd0830888d8c77ede1fc720a6780a272bf04e8e
gerard singian <gerard.singian@outlook.com>
2022-05-22 14:01:31 +0800

    Creating a member edit component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
@@ -17,8 +18,9 @@ const routes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      {path:'members', component: MemberListComponent, canActivate: [AuthGuard]},
+      {path:'members', component: MemberListComponent},
       {path:'members/:username', component: MemberDetailComponent},
+      {path:'member/:edit', component: MemberEditComponent},
       {path:'lists', component: ListsComponent},
       {path:'messages', component: MessagesComponent},
     ]
