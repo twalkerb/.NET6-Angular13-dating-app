89c861a390fd98f6a83055e3717769c9dc5861de
gerard singian <gerard.singian@outlook.com>
2022-05-25 22:21:46 +0800

    Adding a PhotoService

---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudinaryDotNet.Actions;
+
+namespace API.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
