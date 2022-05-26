7e115ccf39ad2c855885fd9c8f8ee0f74303d06b
gerard singian <gerard.singian@outlook.com>
2022-05-26 23:13:17 +0800

    Using the created at route method

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -39,7 +39,7 @@ namespace API.Controllers
 
 
 
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
@@ -83,7 +83,11 @@ namespace API.Controllers
             user.Photos.Add(photo);
 
             if (await _userRepository.SaveAllAsync())
-            return _mapper.Map<PhotoDto>(photo);
+            {
+        
+            return CreatedAtRoute("GetUser", new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+            }
+
 
             return BadRequest("Problem adding Photo");
         }
