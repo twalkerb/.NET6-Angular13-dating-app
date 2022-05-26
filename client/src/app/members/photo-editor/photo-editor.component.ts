901eb0a45d634cefcdf8844c192f40376f0c9aa4
gerard singian <gerard.singian@outlook.com>
2022-05-26 23:32:41 +0800

    Adding a Photo editor Component

---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() member: Member;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
