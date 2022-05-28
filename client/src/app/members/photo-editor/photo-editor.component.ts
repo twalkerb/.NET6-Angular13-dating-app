179395652c57b80c96efedfb151bd06391c20d6a
gerard singian <gerard.singian@outlook.com>
2022-05-29 00:03:04 +0800

    Deleting Photos Client

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -38,8 +38,14 @@ export class PhotoEditorComponent implements OnInit {
       this.member.photoUrl = photo.url;
       this.member.photos.forEach(p => {
         if (p.isMain) p.isMain = false;
-        if (p.id === photo.id) p.isMain = true; 
+        if (p.id === photo.id) p.isMain = true;
       })
+    })
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter(x => x.id !== photoId);
     })
   }
   initializeUploader() {
