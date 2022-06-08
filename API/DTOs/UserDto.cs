772ab9bd9c52b83c36040d9dd1ec7f812fee098c
gerard singian <gerard.singian@outlook.com>
2022-06-08 22:57:58 +0800

    Updating the API register Method

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -10,5 +10,6 @@ namespace API.DTOs
         public string Username { get; set; }
         public string Token { get; set; }
         public string PhotoUrl {get; set;}
+        public string KnownAs {get; set;}
     }
 }
