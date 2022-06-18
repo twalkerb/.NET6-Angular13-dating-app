944a9256623bd38ef0292736612ec685b1b6d8b6
bryanyl <bryanyingling0@gmail.com>
2022-06-18 16:01:02 +0800

    Adding sorting functionality

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -21,5 +21,6 @@ namespace API.Helpers
         public string Gender {get; set;}
         public int MinAge {get; set; } = 18;
         public int MaxAge {get; set; } = 150;
+        public string OrderBy {get; set;} = "lastActive";
     }
 }
