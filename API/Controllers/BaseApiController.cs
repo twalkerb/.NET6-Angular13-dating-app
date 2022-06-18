ea941a0e3deec16a068598f737de0af970d84345
bryanyl <bryanyingling0@gmail.com>
2022-06-18 22:42:32 +0800

    Adding a Action Filter

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var username = resultContext.HttpContext.User.GetUsername();
+            var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
+            var user = await repo.GetUserByUsernameAsync(username);
+            user.LastActive = DateTime.Now;
+            await repo.SaveAllAsync();
+        }
+    }
+}
