c53a0f22ab4d5217093fe3c8d8c218816b3ae4ba
bryanyl <bryanyingling0@gmail.com>
2022-06-18 22:59:33 +0800

    Making the last active  action filter more optional

--- API/Interfaces/ClaimsPrincipleExtensions.cs
+++ API/Interfaces/ClaimsPrincipleExtensions.cs
@@ -10,7 +10,12 @@ namespace API.Interfaces
     {
         public static string GetUsername(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
         }
     }
 }
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -23,7 +23,8 @@ namespace API.Services
         {
             var claims = new List<Claim>
             {
-                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
             };
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256Signature);
@@ -39,7 +40,7 @@ namespace API.Services
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            return tokenHandler.WriteToken(token);  
+            return tokenHandler.WriteToken(token);
         }
     }
 }
