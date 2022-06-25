4c0b330f0f7dba18d8e6921655d333f675f94dd9
bryanyl <bryanyingling0@gmail.com>
2022-06-25 13:20:56 +0800

    Adding the likes component

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -102,7 +102,7 @@ export class MembersService {
   }
 
   getLikes(predicate: string) {
-    return this.http.get(this.baseUrl + 'likes?=' + predicate);
+    return this.http.get<Partial<Member[]>>(this.baseUrl + 'likes?predicate=' + predicate);
   }
 
   private getPaginatedResult<T>(url, params) {
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
+  members: Partial<Member[]>;
+  predicate = 'liked';
 
-  constructor() { }
+  constructor(private memberService: MembersService) {}
 
   ngOnInit(): void {
+    this.loadLikes();
   }
 
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe(response => {
+      this.members = response;
+    })
+  }
 }
