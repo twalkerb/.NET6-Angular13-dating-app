ded3265f6a9f5c462592865c2c0834af35fd5582
bryanyl <bryanyingling0@gmail.com>
2022-06-26 00:18:45 +0800

    Setting up the entities for messaging

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -15,13 +15,14 @@ namespace API.Data
 
         public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
             builder.Entity<UserLike>()
-            .HasKey(k => new {k.SourceUserId, k.LikedUserId });
+            .HasKey(k => new { k.SourceUserId, k.LikedUserId });
 
             builder.Entity<UserLike>()
             .HasOne(s => s.SourceUser)
@@ -34,6 +35,18 @@ namespace API.Data
             .WithMany(l => l.LikedByUsers)
             .HasForeignKey(s => s.LikedUserId)
             .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<Message>()
+            .HasOne(u => u.Recipient)
+            .WithMany(m => m.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.Entity<Message>()
+                   .HasOne(u => u.Sender)
+                   .WithMany(m => m.MessagesSent)
+                   .OnDelete(DeleteBehavior.Restrict);
+
         }
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -25,5 +25,8 @@ namespace API.Entities
         public ICollection<Photo> Photos {get; set;}
        public ICollection<UserLike> LikedByUsers { get; set; }
        public ICollection<UserLike> LikedUsers {get; set;}
+
+       public ICollection<Message> MessagesSent {get; set;}
+       public ICollection<Message> MessagesReceived {get; set;}
     }
 }
