345a4b464e70837be32cded044dd4f29c482c2c8
bryanyl <bryanyingling0@gmail.com>
2022-06-26 01:23:26 +0800

    Setting up the automapper Profiles

---
+++ API/DTOs/CreateMessageDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class CreateMessageDto
+    {
+        public string RecipientUsername {get; set;}
+        public string Content {get; set;}
+    }
+}
