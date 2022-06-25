f481d4a4f198b8ec9eaa67a56e94d9fe62438f9f
bryanyl <bryanyingling0@gmail.com>
2022-06-25 17:51:17 +0800

    Paginating the likes on the client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -101,8 +101,10 @@ export class MembersService {
     return this.http.post(this.baseUrl + 'likes/' + username, {});
   }
 
-  getLikes(predicate: string) {
-    return this.http.get<Partial<Member[]>>(this.baseUrl + 'likes?predicate=' + predicate);
+  getLikes(predicate: string, pageNumber, pageSize) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes', params);
   }
 
   private getPaginatedResult<T>(url, params) {
