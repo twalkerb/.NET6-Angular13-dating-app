219f2cda1cf93b45f6a5a8c380a704ad0cc51f41
bryanyl <bryanyingling0@gmail.com>
2022-07-04 23:19:32 +0800

    Adding an admin component Front end

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
@@ -25,6 +26,7 @@ const routes: Routes = [
       {path:'member/:edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
       {path:'lists', component: ListsComponent},
       {path:'messages', component: MessagesComponent},
+      {path:'admin', component: AdminPanelComponent},
     ]
   },
   {path: 'errors', component: TestErrorsComponent},
