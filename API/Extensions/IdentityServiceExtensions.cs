30cf600b6af4d27d2fcea997e2fe6f0551cfd282
bryanyl <bryanyingling0@gmail.com>
2022-07-04 22:23:51 +0800

    Adding a policy based on the authorisation

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -39,6 +39,12 @@ namespace API.Extensions
                     };
                 });
 
+            services.AddAuthorization(opt => 
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
                 return services;
 
         }
