6db434ef0b77c67842d322ecdf22a37fafd6924a
bryanyl <bryanyingling0@gmail.com>
2022-07-10 01:43:18 +0800

    Addinng a presence tracker

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -8,12 +8,15 @@ using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
+using API.SignalR;
+
 namespace API.Extensions
 {
     public static class ApplicationServiceExtensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
+            services.AddSingleton<PresenceTracker>();
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IPhotoService, PhotoService>();
--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -11,14 +11,28 @@ namespace API.SignalR
     [Authorize]
     public class PresenceHub : Hub
     {
+        private readonly PresenceTracker _tracker;
+        public PresenceHub(PresenceTracker tracker)
+        {
+            _tracker = tracker;
+        }
+
         public override async Task OnConnectedAsync()
         {
+            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UsersIsOnline", Context.User.GetUsername());
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
+                var currentUsers = await _tracker.GetOnlineUsers();
+                await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
 
             await base.OnDisconnectedAsync(exception);
         }
