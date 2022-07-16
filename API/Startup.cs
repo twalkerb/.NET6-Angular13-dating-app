134813b6f669ac3a1421cece93c7e04c5086b5f1
bryanyl <bryanyingling0@gmail.com>
2022-07-16 19:32:48 +0800

    Creating a messsage hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -81,6 +81,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
