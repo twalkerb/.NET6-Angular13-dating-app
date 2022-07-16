e8ee10e70490f3dc18872166e4e0fcdece94215d
bryanyl <bryanyingling0@gmail.com>
2022-07-17 01:14:03 +0800

    Tracking the message Groups

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -20,6 +20,8 @@ namespace API.Data
         // public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<Group> Groups {get; set;}
+        public DbSet<Connection> Connections {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
